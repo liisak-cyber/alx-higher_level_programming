37f0e9e35e0bb65ab036a8356c240d82f5f28313
liisak-cyber <liisakmodels@gmail.com>
2024-02-19 02:55:22 -0800

    Python Input and Output

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,5 +1,5 @@
-0-read_file.py
 #!/usr/bin/python3
+
 """Defines a class Student."""
 
 
@@ -8,7 +8,6 @@ class Student:
 
     def __init__(self, first_name, last_name, age):
         """Initialize a new Student.
-
         Args:
             first_name (str): The first name of the student.
             last_name (str): The last name of the student.
@@ -20,10 +19,8 @@ class Student:
 
     def to_json(self, attrs=None):
         """Get a dictionary representation of the Student.
-
         If attrs is a list of strings, represents only those attributes
         included in the list.
-
         Args:
             attrs (list): (Optional) The attributes to represent.
         """
@@ -34,7 +31,6 @@ class Student:
 
     def reload_from_json(self, json):
         """Replace all attributes of the Student.
-
         Args:
             json (dict): The key/value pairs to replace attributes with.
         """
