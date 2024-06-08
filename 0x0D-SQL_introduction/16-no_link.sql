9ed3e00e52b9390f9148b5d81afabdd1be1f4ac0
liisak-cyber <liisakmodels@gmail.com>
2024-06-08 00:22:32 -0700

    MySQL_intro after bugFix

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,6 @@
-SELECT score, name
-FROM second_table
-HAVING name IS NOT NULL
-ORDER BY score DESC;
+-- Lists the number of records wit the same score in the table second_table.
+-- Records are ordered by descending count.
+SELECT `score`, COUNT(*) AS `number`
+FROM `second_table`
+GROUP BY  name`score`
+ORDER BY `number` DESC;
