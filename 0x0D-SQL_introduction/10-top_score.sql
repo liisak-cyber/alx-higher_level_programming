32d33822b6a08addb138700d90cafad8dcbc0859
liisak-cyber <liisakmodels@gmail.com>
2024-06-07 23:48:01 -0700

    MySQL_intro after bugFix

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,1 +1,3 @@
-SELECT score, name FROM second_table ORDER BY score DESC;
+-- Lists all records of the table second_table.
+-- Records are ordered by descending score.
+SELECT `score`, `name` FROM `second_table` ORDER BY `score` DESC;
