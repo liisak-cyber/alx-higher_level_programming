30b47a96ec2738a3dd29efc406e6ba18c96a8d5a
liisak-cyber <liisakmodels@gmail.com>
2024-06-11 12:41:05 -0700

    SQL_Introduction Advanced tasks

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,6 @@
 -- Lists the number of records wit the same score in the table second_table.
 -- Records are ordered by descending count.
-SELECT `score`, COUNT(*) AS `number`
+SELECT `score`, `name`
 FROM `second_table`
-GROUP BY  name`score`
-ORDER BY `number` DESC;
+HAVING `name` IS NOT NULL
+ORDER BY `score` DESC;
