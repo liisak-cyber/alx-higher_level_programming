72a970713c38dc752d3a4a0fff4deaccb97fc043
liisak-cyber <liisakmodels@gmail.com>
2024-06-13 09:06:10 -0700

    SQL_more_queries

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,24 @@
+-- Creates a database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa)
+
+-- Create the database if it does not exist
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+
+-- Use the created database
+USE `hbtn_0d_usa`;
+
+-- Create the states table if it does not exist
+CREATE TABLE IF NOT EXISTS `states` (
+    `id` INT AUTO_INCREMENT NOT NULL,
+    `name` VARCHAR(256) NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+-- Create the cities table if it does not exist
+CREATE TABLE IF NOT EXISTS `cities` (
+    `id` INT AUTO_INCREMENT NOT NULL,
+    `state_id` INT NOT NULL,
+    `name` VARCHAR(256) NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`state_id`) REFERENCES `states`(`id`)
+);
+
