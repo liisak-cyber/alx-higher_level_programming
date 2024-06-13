0e94e64d3b70423dfa7ccbaebfe482a1f2e30718
liisak-cyber <liisakmodels@gmail.com>
2024-06-13 14:36:23 -0700

    SQL_more_queries

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,15 @@
+-- List all genres and the number of shows linked to each, sorted by number of shows in descending order
+
+SELECT
+    tv_genres.name AS genre,
+    COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM
+    tv_genres
+INNER JOIN
+    tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY
+    tv_genres.name
+HAVING
+    number_of_shows > 0
+ORDER BY
+    number_of_shows DESC;
