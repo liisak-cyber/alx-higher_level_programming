3e818e0634e8446689ede30cc3021ee5652939cd
liisak-cyber <liisakmodels@gmail.com>
2024-06-13 08:57:41 -0700

    Corrected 6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,12 +1,14 @@
--- Creates a database hbtn_0d_usa and the table states
+-- Creates a database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
 
--- Create the database
+-- Create the database if it does not exist
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 
--- Create the table within the hbtn_0d_usa database
+-- Use the created database
 USE `hbtn_0d_usa`;
 
+-- Create the table if it does not exist
 CREATE TABLE IF NOT EXISTS `states` (
-    `id` INT NOT NULL,
-    `name` VARCHAR(256)
+    `id` INT AUTO_INCREMENT NOT NULL,
+    `name` VARCHAR(256) NOT NULL,
+    PRIMARY KEY (`id`)
 );
