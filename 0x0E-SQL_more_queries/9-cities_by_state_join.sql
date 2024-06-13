698a97511e75e28a361b43b5c747ef444275e99b
liisak-cyber <liisakmodels@gmail.com>
2024-06-13 09:40:16 -0700

    SQL_more_queries

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -3,8 +3,11 @@
 SELECT 
     `cities`.`id`,
     `cities`.`name`,
-    (SELECT `states`.`name` FROM `states` WHERE `states`.`id` = `cities`.`state_id`) AS `state_name`
+    `states`.`name` AS `state_name`
 FROM 
-    `cities`
+    `cities`,
+    `states`
+WHERE
+    `cities`.`state_id` = `states`.`id`
 ORDER BY 
     `cities`.`id` ASC;
