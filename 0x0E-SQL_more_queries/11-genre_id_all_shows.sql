097274ddcbf639f9fcff9ba7aceab577c1ef0051
liisak-cyber <liisakmodels@gmail.com>
2024-06-13 10:01:47 -0700

    SQL_more_queries

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,13 +1,13 @@
 -- List all shows with their linked genre_ids or NULL if no genre is linked, sorted correctly
 
-SELECT 
-    tv_shows.title,
-    IFNULL(GROUP_CONCAT(tv_show_genres.genre_id ORDER BY tv_show_genres.genre_id SEPARATOR ', '), 'NULL') AS genre_ids
+SELECT
+	tv_shows.title,
+	IFNULL(GROUP_CONCAT(tv_show_genres.genre_id ORDER BY tv_show_genres.genre_id SEPARATOR ', '), 'NULL') AS genre_ids
 FROM 
-    tv_shows
-LEFT JOIN 
-    tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-GROUP BY 
-    tv_shows.title
-ORDER BY 
-    tv_shows.title ASC;
+	tv_shows
+LEFT JOIN
+	tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+GROUP BY
+	tv_shows.title
+ORDER BY
+	tv_shows.title ASC;
