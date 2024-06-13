39fe152503563e3e319acd02afd539f452453306
liisak-cyber <liisakmodels@gmail.com>
2024-06-13 09:22:42 -0700

    SQL_more_queries

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,10 +1,10 @@
--- Lists all the cities of California in the database hbtn_0d_usa
+-- List all cities of California in the database hbtn_0d_usa
 
-SELECT `cities`.`id`, `cities`.`names`
+SELECT `cities`.`id`, `cities`.`name`
 FROM `cities`
-WHERE `cities`.`state_id` = (    
-	SELECT `states`.`id`
-	FROM `states`
-	WHERE `states`.`name` = 'California'
+WHERE `cities`.`state_id` = (
+    SELECT `states`.`id`
+    FROM `states`
+    WHERE `states`.`name` = 'California'
 )
 ORDER BY `cities`.`id` ASC;
