9d45b35803ab7db606b0027dbc6e30a6b07a933c
liisak-cyber <liisakmodels@gmail.com>
2024-06-13 09:26:59 -0700

    SQL_more_queries

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,10 @@
+-- List all cities with their state names from the database hbtn_0d_usa
+
+SELECT 
+    `cities`.`id`,
+    `cities`.`name`,
+    (SELECT `states`.`name` FROM `states` WHERE `states`.`id` = `cities`.`state_id`) AS `state_name`
+FROM 
+    `cities`
+ORDER BY 
+    `cities`.`id` ASC;
