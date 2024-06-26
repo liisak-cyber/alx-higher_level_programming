8d06f032c40ab4cf2b08f8986d4c0057553f41b6
liisak-cyber <liisakmodels@gmail.com>
2024-06-26 03:51:16 -0700

    Add initial JavaScript scripts for tasks 0 to 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,7 @@
+#!/usr/bin/node
 const args = process.argv.slice(2);
-if (args[0] === undefined) {
-  console.log("No argument");
+if (args.length === 0) {
+  console.log('No argument');
 } else {
   console.log(args[0]);
 }
