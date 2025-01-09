00133fdd8fd27c469b4dcbc696a608851b33a5a7
liisak-cyber <liisakmodels@gmail.com>
2025-01-09 11:56:07 -0800

    Object Relational Mapping

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -1,15 +1,17 @@
-# 6-model_state.py
 #!/usr/bin/python3
 """
-Creates the states table in the database
+Creates the 'states' table in the MySQL database.
 """
+
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
 
 if __name__ == "__main__":
-        # Connect to MySQL
-            engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    # Create the connection engine to MySQL
+    engine = create_engine(
+                            'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True
+                            )
 
-                # Create all tables
-                    Base.metadata.create_all(engine)
+    # Create all tables in the database (including 'states')
+    Base.metadata.create_all(engine)
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,14 +1,22 @@
-# model_state.py
+#!/usr/bin/python3
+"""
+Defines the State class that links to the 'states' table in MySQL.
+"""
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+# Create the base class using declarative_base()
 Base = declarative_base()
 
 class State(Base):
-        """
-            Class that defines a state.
-                """
-                    __tablename__ = 'states'
+    """
+    Class that defines a State.
+    The class links to the 'states' table in MySQL database.
+    """
 
-                        id = Column(Integer, unique=True, nullable=False, primary_key=True)
-                            name = Column(String(128), nullable=False)
+    __tablename__ = 'states'
+
+# Define the columns for the states table
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
